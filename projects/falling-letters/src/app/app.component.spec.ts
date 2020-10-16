d1eafb73cd8b1d5e0520f09c865b4c4111f75404
Christoph Murczek <christoph.murczek@outlook.com>
2020-10-16 07:40:23 +0200

    test: uses material component test harnesses

--- projects/falling-letters/src/app/app.component.spec.ts
+++ projects/falling-letters/src/app/app.component.spec.ts
@@ -1,33 +1,41 @@
-import { waitForAsync, TestBed } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarHarness } from '@angular/material/toolbar/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
+let loader: HarnessLoader;
+let fixture: ComponentFixture<AppComponent>;
 
 describe('AppComponent', () => {
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
-        imports: [RouterTestingModule],
+        imports: [RouterTestingModule, NoopAnimationsModule, MatToolbarModule],
         declarations: [AppComponent],
       }).compileComponents();
+      fixture = TestBed.createComponent(AppComponent);
+      loader = TestbedHarnessEnvironment.loader(fixture);
     })
   );
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shell'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'Typing Games'`, () => {
+    // const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shell');
+    expect(app.title).toEqual('Typing Games');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('shell app is running!');
+  it('should render title', async () => {
+    const toolbarHarness = await loader.getHarness(MatToolbarHarness);
+    const title = await toolbarHarness.getRowsAsText();
+    expect(title).toContain('Fallende Buchstaben');
   });
 });
