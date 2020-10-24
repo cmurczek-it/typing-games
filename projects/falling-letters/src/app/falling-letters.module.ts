04b891d3a46e178872b4e9dd2b320cf6cc09996a
Christoph Murczek <christoph.murczek@outlook.com>
2020-10-25 01:54:06 +0200

    fix: fixes imports in module

--- projects/falling-letters/src/app/falling-letters.module.ts
+++ projects/falling-letters/src/app/falling-letters.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { fallingLetterReducer } from './++state/reducer';
 import { FallingCharacterComponent } from './falling-character/falling-character.component';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [ShellComponent, GameBoardComponent, FallingCharacterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    StoreModule.forRoot(fallingLetterReducer, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+  ],
   providers: [],
   bootstrap: [ShellComponent],
 })
